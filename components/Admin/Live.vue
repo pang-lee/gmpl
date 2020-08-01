195612331904c8ddd1b24165c401c55e3c7f2bd2
Shih-Pnag <leebond88@yahoo.com.tw>
2020-08-01 19:19:07 +0800

    change

--- components/Admin/Live.vue
+++ components/Admin/Live.vue
@@ -9,7 +9,21 @@
         Live Stream On Restream&nbsp;<v-icon>{{ stream }}</v-icon>
       </v-btn>
       <v-card v-else>
-        <h1 class="font-weight-black font-italic"> Restream UserName: {{ restream.username }}  Restream Email: {{ restream.email }} </h1>
+        <v-col>
+          <v-list>
+            <v-subheader class="d-flex justify-center">Restream UserName:&nbsp;&nbsp;{{ restream.username }}</v-subheader>
+            <v-list-item-group color="primary">
+              <v-list-item v-for="chan in channel" :key="chan.id">
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ chan.displayName }} - <a :href="chan.url">{{ chan.url }}</a>
+                    <v-switch inset class="ml-3" v-model="chan.enabled" :label="`Status: ${chan.enabled == true ? 'active' : 'inactive'}`" @click="switchStatus(chan.enabled, chan.id)"></v-switch>
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-col>
       </v-card>
     </v-row>
   </v-container>
@@ -25,18 +39,26 @@ export default class Restream extends Vue{
   data(){
     return{
       stream:icon.mdiVideoBox,
-      restream:{}
+      restream:{},
+      channel:[]
     }
   }
 
   get code(){
     if(this.$route.fullPath.indexOf('code') == -1) return true
-    else {
+    else{
       admin.get_token(this.$route.fullPath.substring(this.$route.fullPath.indexOf('=') + 1, this.$route.fullPath.indexOf('&')))
       this.restream = admin.restreamProfile
+      this.channel = admin.restreamChannel
       return false
     }
   }
+
+  switchStatus(status, identifier){
+    if(status == false) return admin.change_status({ state:true, id:identifier })
+    else return admin.change_status({ state:false, id:identifier })
+  }
+
 }
 </script>
 
--- store/admin.ts
+++ store/admin.ts
@@ -1,4 +1,4 @@
-import { Module, Mutation, Action, VuexModule, getModule } from 'vuex-module-decorators'
+import { Module, Mutation, Action, VuexModule } from 'vuex-module-decorators'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
@@ -45,9 +45,9 @@ export default class Admin extends VuexModule {
 
     public items:Array<String> = ['Profile', 'Live', 'Friend', 'Post', 'Analysis']
 
-    public restream_accessToken:string = ''
-
     public restream_profile:Object = {}
+
+    public restream_channel:Array<Object> = []
 
     public get self_info():any{
         return this.info
@@ -61,12 +61,12 @@ export default class Admin extends VuexModule {
       return this.info_item_number
     }
 
-    public get restreamAccess():string{
-        return this.restream_accessToken
-    }
-
     public get restreamProfile():Object{
         return this.restream_profile
+    }
+
+    public get restreamChannel():Array<Object>{
+        return this.restream_channel
     }
 
     @Mutation
@@ -100,42 +100,77 @@ export default class Admin extends VuexModule {
     }
 
     @Mutation
-    public set_restreamToken(token:string):string{
-        return this.restream_accessToken = token
-    }
-
-    @Mutation
     public set_profile(profile:Object):Object{
         return this.restream_profile = profile
     }
 
+    @Mutation
+    public set_channel(channel:Array<Object>):Array<Object>{
+        return this.restream_channel = channel
+    }
+
     @Action({ rawError:true })
-    async get_token(code:String){
-        try{
-            const OAUTH:string = String(process.env.CSRF_ANYWHERE) + String(process.env.RESTREAM_OAUTH)
-            const PROFILE:string = String(process.env.CSRF_ANYWHERE) + String(process.env.RESTREAM_PROFILE)
-            const uname:string = String(process.env.RESTREAM_CLIENT)
-            const pass:string = String(process.env.RESTREAM_SECRET)
-            const oauth:any = await axios.post(OAUTH, Qs.stringify({ grant_type: 'authorization_code', redirect_uri: process.env.BASE_URL + '/admin', code: code }), { auth:{ username: uname, password: pass }, headers:{ 'Content-Type': 'application/x-www-form-urlencoded' } })
-            console.log(oauth.data)
-            this.context.commit('set_restreamToken', oauth.data.access_token)
-            localStorage.setItem('restram_accessToken', oauth.data.access_token)
-            localStorage.setItem('restream_expires', String(oauth.data.expires))
-            cookies.set('restram_accessToken', oauth.data.access_token)
-            cookies.set('restream_expires', oauth.data.expires)
-            setTimeout(()=>{
-                cookies.remove('restram_accessToken')
-                cookies.remove('restream_expires')
-                if (process.client) {
-                  localStorage.removeItem('restram_accessToken')
-                  localStorage.removeItem('restream_expires')
-                }
-            }, oauth.data.expires)
-            const profile:any = await axios.get(PROFILE, { headers:{ authorization: 'Bearer ' + this.context.getters.restreamAccess } })
-            this.context.commit('set_profile', profile.data)
-            console.log(this.context.getters.restreamProfile)
-        }catch(error){
-            console.log(error)
+    async get_token(code:String):Promise<any>{
+        if(!(localStorage.getItem('restram_accessToken') && cookies.get('restram_accessToken'))){
+            try{
+                const OAUTH:string = String(process.env.CSRF_ANYWHERE) + String(process.env.RESTREAM_OAUTH)
+                const uname:string = String(process.env.RESTREAM_CLIENT)
+                const pass:string = String(process.env.RESTREAM_SECRET)
+                const oauth:any = await axios.post(OAUTH, Qs.stringify({ grant_type: 'authorization_code', redirect_uri: process.env.BASE_URL + '/admin', code: code }), { auth:{ username: uname, password: pass }, headers:{ 'Content-Type': 'application/x-www-form-urlencoded' } })
+                localStorage.setItem('restram_accessToken', oauth.data.access_token)
+                localStorage.setItem('restream_expires', String(new Date().getTime() + Number.parseInt(oauth.data.expires)))
+                cookies.set('restram_accessToken', oauth.data.access_token)
+                cookies.set('restream_expires', new Date().getTime() + Number.parseInt(oauth.data.expires))
+                setTimeout(()=>{
+                    cookies.remove('restram_accessToken')
+                    cookies.remove('restream_expires')
+                    if (process.client) {
+                      localStorage.removeItem('restram_accessToken')
+                      localStorage.removeItem('restream_expires')
+                    }
+                }, oauth.data.expires)
+                this.context.dispatch('get_profile', cookies.get('restram_accessToken'))
+                this.context.dispatch('get_channel', cookies.get('restram_accessToken'))
+            }catch(error){
+                console.log(error)
+            }
+        }else{
+            this.context.getters.restreamProfile
+            this.context.getters.restreamChannel
         }
-      }
+    }
+
+    @Action({ rawError:true })
+    public async get_profile(token:string):Promise<any>{
+        try{
+            const PROFILE:string = String(process.env.CSRF_ANYWHERE) + String(process.env.RESTREAM_PROFILE)
+            const profile:any = await axios.get(PROFILE, { headers:{ authorization: 'Bearer ' + token } })
+            this.context.commit('set_profile', profile.data)
+        }catch(error){
+            console.log("profile error " + error)
+        }
+    }
+
+    @Action({ rawError:true })
+    public async get_channel(token:string):Promise<any>{
+        try{
+            const CHANNEL:string = String(process.env.CSRF_ANYWHERE) + String(process.env.RESTREAM_CHANNEL)
+            const channel:any = await axios.get(CHANNEL, { headers:{ authorization: 'Bearer ' + token } })
+            this.context.commit('set_channel', channel.data)
+            console.log(this.context.getters.restreamChannel)
+        }catch(error){
+            console.log("channel error " + error)
+        }
+    }
+
+    @Action({ rawError:true })
+    public async change_status({ state, id }:any):Promise<any>{
+        try{
+            const STATUS:string = String(process.env.CSRF_ANYWHERE) + String(process.env.RESTREAM_STATUS)
+            const status:any = await axios.patch(STATUS + id, JSON.stringify({ active:state }), { headers:{ authorization: 'Bearer ' + cookies.get('restram_accessToken'), 'Content-Type': 'application/json' } })
+            console.log(status)
+        }catch(error){
+            console.log("status error " + error)
+        }
+    }
 }
