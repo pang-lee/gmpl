678faf64c041c6b79145653b2695c0970b0947fd
Shih-Pnag <leebond88@yahoo.com.tw>
2020-08-08 12:27:23 +0800

    cc

---
+++ plugins/videoplayer.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VideoPlayer from 'vue-video-player'
+import '~/node_modules/video.js/dist/video-js.css'
+import '~/node_modules/vue-video-player/src/custom-theme.css'
+import '~/node_modules/videojs-contrib-hls'
+Vue.use(VideoPlayer)
